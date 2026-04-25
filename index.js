3dc266e36624b34fa19878cc40b3ef191f8c9166
vipra-b <88008387+vipra-b@users.noreply.github.com>
2026-04-24 20:30:55 -0700

    add fpro project

---
+++ index.js
@@ -0,0 +1,28 @@
+import { fetchJSON, renderProjects, fetchGitHubData } from './global.js';
+
+const projects = await fetchJSON('./lib/projects.json');
+const latestProjects = projects.slice(0, 3);
+
+const projectsContainer = document.querySelector('.projects');
+
+renderProjects(latestProjects, projectsContainer, 'h2');
+
+const githubData = await fetchGitHubData('vipra-b');
+const profileStatsContent = document.querySelector(
+  '#profile-stats .profile-stats__content',
+);
+
+if (profileStatsContent && githubData) {
+  profileStatsContent.innerHTML = `
+    <dl>
+      <dt>Public Repos</dt>
+      <dt>Public Gists</dt>
+      <dt>Followers</dt>
+      <dt>Following</dt>
+      <dd>${githubData.public_repos}</dd>
+      <dd>${githubData.public_gists}</dd>
+      <dd>${githubData.followers}</dd>
+      <dd>${githubData.following}</dd>
+    </dl>
+  `;
+}
